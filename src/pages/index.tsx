96f5f70df9c1926af49281c71c42024959d2018c
Danny Libin <dlibinrx@gmail.com>
2021-04-14 16:51:11 -0500

    index seo added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import { useRouter } from 'next/dist/client/router';
 import React, { useEffect } from 'react';
+import SEO from '../components/seo';
 
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
     router.push('/simulator');
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      <SEO
+        title="FI Portfolio Doctor Simulator"
+        description="An app for projecting portfolio performance for financial independence and retirement"
+      />
+    </div>
+  );
 }
